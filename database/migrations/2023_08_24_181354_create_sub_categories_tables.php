b15e56da827b175104045987763bb0e4dcfb797f
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-08-27 16:26:53 +0000

    Create 2023_08_24_181354_create_sub_categories_tables.php

---
+++ database/migrations/2023_08_24_181354_create_sub_categories_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->string('sub_cat_id')->primary();
+            $table->string('name')->unique();
+            $table->string('image');
+            $table->string('cat_id');
+            $table->timestamps();
+
+            $table->foreign('cat_id')->references('cat_id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+-        Schema::dropIfExists('sub_categories');
+    }
+};
