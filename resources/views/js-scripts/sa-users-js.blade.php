f5f4a36376547c9fc0a1829ca8cbcbba90c340b5
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-08-27 12:23:30 +0000

    Update sa-users-js.blade.php

--- resources/views/js-scripts/sa-users-js.blade.php
+++ resources/views/js-scripts/sa-users-js.blade.php
@@ -1,23 +1,5 @@
 <script type="text/javascript">
 
-    // constant to get all roles
-    const AllUsersRoles = () =>{
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            url:'{{route('sa.get-all-roles')}}',
-            method:'GET',
-            cache:false,
-            success:(Response)=>{
-                $("#edit-user-modal-form").find("select[name=role]").append(Response)
-            }
-        })
-    }
-    AllUsersRoles();
-
     // Add new user
     $("#sa-new-user-form #new-user-form-alert").hide()
     $(document).on("submit", "#sa-new-user-form", (e)=>{
