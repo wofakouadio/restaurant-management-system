7c3bc7e51540f378b6384801bfdbc2f75ebf6c47
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-08-29 13:08:43 +0000

    Update Menu.php

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'menu_id',
+        'name',
+        'description',
+        'size',
+        'extra',
+        'price',
+        'discount',
+        'category',
+        'subcategory',
+        'review',
+        'status',
+        'image'
+    ];
+
+    protected $table = 'menus';
 }
