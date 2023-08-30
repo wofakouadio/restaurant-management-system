fead2c9ab9dd830ff94add578db12ac3aafc6c78
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-08-30 12:44:54 +0000

    Update sa-functions-js.blade.php

--- resources/views/js-scripts/sa-functions-js.blade.php
+++ resources/views/js-scripts/sa-functions-js.blade.php
@@ -32,26 +32,27 @@
                 $("#add-sub-category-modal").find("select[name=cat-id]").append(Response)
                 $("#edit-sub-category-modal").find("select[name=cat-id]").append(Response)
                 $("#sa-new-menu-form").find("select[name=cat-id]").append(Response)
+                $("#edit-menu-modal").find("select[name=cat-id]").append(Response)
             }
         })
     }
     AllCategoriesInDropdown()
 
-    function SubCategoriesBasedOnCategory(category_id){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            url:'{{route('sa.sub-categories-dropdown-based-on-category')}}',
-            method:'GET',
-            cache:false,
-            data:{'cat-id': category_id},
-            success:(Response)=>{
-                console.log(Response)
-            }
-        })
-    }
+    {{--function SubCategoriesBasedOnCategory(category_id){--}}
+    {{--    $.ajaxSetup({--}}
+    {{--        headers: {--}}
+    {{--            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
+    {{--        }--}}
+    {{--    });--}}
+    {{--    $.ajax({--}}
+    {{--        url:'{{route('sa.sub-categories-dropdown-based-on-category')}}',--}}
+    {{--        method:'GET',--}}
+    {{--        cache:false,--}}
+    {{--        data:{'cat-id': category_id},--}}
+    {{--        success:(Response)=>{--}}
+    {{--            console.log(Response)--}}
+    {{--        }--}}
+    {{--    })--}}
+    {{--}--}}
 
 </script>
