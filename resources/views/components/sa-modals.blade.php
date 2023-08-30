d9adbbc2f4c351c99761b2de14f19ef99c897d60
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-08-30 12:44:49 +0000

    Update sa-modals.blade.php

--- resources/views/components/sa-modals.blade.php
+++ resources/views/components/sa-modals.blade.php
@@ -479,11 +479,11 @@
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="block block-rounded shadow-none mb-0">
-                <form method="POST" enctype="multipart/form-data" id="sa-update-sub-category-form">
+                <form method="POST" enctype="multipart/form-data" id="sa-update-menu-form">
                     @csrf
                     @method('PUT')
                     <div class="block-header block-header-default">
-                        <h3 class="block-title">Sub-Category</h3>
+                        <h3 class="block-title">Menu</h3>
                         <div class="block-options">
                             <button type="button" class="btn-block-option" data-bs-dismiss="modal" aria-label="Close">
                                 <i class="fa fa-times"></i>
@@ -491,25 +491,96 @@
                         </div>
                     </div>
                     <div class="block-content block-content-full fs-sm">
-                        <div class="alert alert-danger sub-category-alert"></div>
-                        <div class="form-group mb-4">
-                            <label class="form-label" for="mega-firstname">Name</label>
-                            <input type="text" class="form-control form-control-lg" id="mega-firstname" name="name" placeholder="Enter category..">
-                            <span class="text-danger" id="sub-category-name-err"></span>
-                            <input type="hidden" name="sub_cat_id">
+                        <div class="alert alert-danger menu-alert"></div>
+                        <div class="row mb-4">
+                            <div class="col">
+                                <label class="form-label">Name</label>
+                                <input type="text" class="form-control form-control-lg" name="name" placeholder="Enter your menu..">
+                                <input name="menu_id" type="hidden">
+                                <span class="text-danger" id="name-err"></span>
+                            </div>
+                            <div class="col">
+                                <label class="form-label">Category</label>
+                                <select name="cat-id" class="js-select2 form-control form-control-lg" style="width: 100%;" data-container="#edit-menu-modal" data-placeholder="Choose one..">
+                                    <option></option>
+                                </select>
+                                <span class="text-danger" id="category-err"></span>
+                            </div>
+                            <div class="col">
+                                <label class="form-label">Sub-Category</label>
+                                <select name="sub-cat-id" class="js-select2 form-control form-control-lg" style="width: 100%;" data-container="#edit-menu-modal" data-placeholder="Choose one..">
+                                    <option value="">Choose</option>
+                                </select>
+                                <span class="text-danger" id="sub-category-err"></span>
+                            </div>
                         </div>
-                        <div class="img cat-profile-picture rounded text-center"></div>
-                        <div class="form-group mb-4">
-                            <label class="form-label" for="example-file-input">Upload Profile Picture</label>
-                            <input class="form-control form-control-lg" type="file" id="example-file-input" name="profile-picture">
-                            <input type="hidden" name="fetched-image">
+                        <div class="row mb-4">
+                            <div class="col">
+                                <label class="form-label">Description</label>
+                                <textarea name="description" class="form-control form-control-lg" cols="10" rows="5"></textarea>
+                                <span class="text-danger" id="description-err"></span>
+                            </div>
+                            <div class="col">
+                                <label class="form-label">Extra</label>
+                                <textarea name="extra" class="form-control form-control-lg" cols="10" rows="5"></textarea>
+                                <span class="text-danger" id="extra-err"></span>
+                            </div>
+                            <div class="col">
+                                <div class="mb-4">
+                                    <label>Pricing</label>
+                                    <input type="text" class="form-control form-control-lg" name="price">
+                                    <span class="text-danger" id="price-err"></span>
+                                </div>
+                                <div class="mb-4">
+                                    <label>Discount</label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control form-control-lg" name="discount">
+                                        <span class="input-group-text">
+                                        <b>%</b>
+                                    </span>
+                                    </div>
+                                    <span class="text-danger" id="discount-err"></span>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group mb-4">
-                            <label class="form-label" for="mega-firstname">Category</label>
-                            <select class="form-control form-control-lg select2" name="cat-id">
-                                <option value="">Choose</option>
-                            </select>
-                            <span class="text-danger" id="category-name-err"></span>
+                        <div class="row mb-4">
+                            <div class="col">
+                                <div class="mb-4">
+                                    <label class="form-label">Size</label>
+                                    <div class="space-x-2">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" value="Small" id="example-checkbox-inline1" name="size[]">
+                                            <label class="form-check-label" for="example-checkbox-inline1">Small</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" value="Medium" id="example-checkbox-inline2" name="size[]">
+                                            <label class="form-check-label" for="example-checkbox-inline2">Medium</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" value="Large" id="example-checkbox-inline3" name="size[]">
+                                            <label class="form-check-label" for="example-checkbox-inline3">Large</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" value="XLarge" id="example-checkbox-inline4" name="size[]">
+                                            <label class="form-check-label" for="example-checkbox-inline4">XLarge</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <label class="form-label" for="example-file-input">Upload Profile Picture</label>
+                                <input class="form-control form-control-lg" type="file" id="example-file-input" name="profile-picture">
+                                <input type="hidden" name="fetched-picture">
+                            </div>
+                            <div class="col">
+                                <label class="form-label" for="mega-role">Status</label>
+                                <select class="form-select form-control form-control-lg" name="status">
+                                    <option value="">Choose</option>
+                                    <option value="1">Available</option>
+                                    <option value="2">Out</option>
+                                </select>
+                                <span class="text-danger" id="status-err"></span>
+                            </div>
                         </div>
                     </div>
                     <div class="block-content block-content-full block-content-sm text-end border-top">
