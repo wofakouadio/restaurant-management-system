7d0d9bba71aa79dbf443572c939efeb7b8086519
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-09-16 11:17:57 +0000

    Update breadcrumb.blade.php

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -58,4 +58,14 @@
             </nav>
         </div>
     </div>
+@elseif(url()->current() === route('sa.new-order'))
+    <div class="bg-body-light border-bottom">
+        <div class="content py-1 text-center">
+            <nav class="breadcrumb bg-body-light py-2 mb-0">
+                <span class="breadcrumb-item">Home</span>
+                <span class="breadcrumb-item">Orders</span>
+                <a class="breadcrumb-item active text-warning" href="{{route('sa.new-order')}}">New Order</a>
+            </nav>
+        </div>
+    </div>
 @endif
