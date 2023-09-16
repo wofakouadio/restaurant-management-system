2770336ff5d625cf5dc07a6b5cfd0bc276faf30c
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-09-16 22:27:56 +0000

    Update app-footer.blade.php

--- resources/views/components/app-footer.blade.php
+++ resources/views/components/app-footer.blade.php
@@ -46,6 +46,6 @@
 @include('js-scripts.sa-categories-js')
 @include('js-scripts.sa-sub-categories-js')
 @include('js-scripts.sa-menus-js')
-    @include('js-scripts.sa-orders-js')
+@include('js-scripts.sa-orders-js')
 </body>
 </html>
