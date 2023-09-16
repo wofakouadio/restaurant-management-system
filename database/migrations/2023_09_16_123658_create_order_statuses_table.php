22e57b367f7342df6d76f7109a6560d91f03dcfe
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-09-16 12:44:00 +0000

    Create 2023_09_16_123658_create_order_statuses_table.php

---
+++ database/migrations/2023_09_16_123658_create_order_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id');
+            $table->tinyInteger('placed_status');
+            $table->dateTime('placed_status_timestamp');
+            $table->tinyInteger('confirmed_status');
+            $table->dateTime('confirmed_status_timestamp');
+            $table->tinyInteger('ready_status');
+            $table->dateTime('ready_status_timestamp');
+            $table->tinyInteger('delivered_status');
+            $table->dateTime('delivered_status_timestamp');
+            $table->tinyInteger('cancelled_status');
+            $table->dateTime('cancelled_status_timestamp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_statuses');
+    }
+};
