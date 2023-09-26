6d95b5454bb7b70724d174f3839a9f23c7cccff0
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-09-26 18:52:31 +0000

    Update app-footer.blade.php

--- resources/views/components/app-footer.blade.php
+++ resources/views/components/app-footer.blade.php
@@ -4,9 +4,7 @@
     Core libraries and functionality
     webpack is putting everything together at assets/_js/main/app.js
 -->
-<script src="{{asset('assets/js/lib/jquery.min.js')}}">
-
-</script>
+<script src="{{asset('assets/js/lib/jquery.min.js')}}"></script>
 <script src="{{asset('assets/js/codebase.app.min.js')}}"></script>
 
 <!-- Page JS Plugins -->
