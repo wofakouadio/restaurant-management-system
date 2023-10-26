1374b8f8ac3e426194e447907ee0913d68f0363e
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-10-26 12:14:34 +0000

    Create admin-app.blade.php

---
+++ resources/views/layouts/admin-app.blade.php
@@ -0,0 +1,39 @@
+<x-app-header/>
+<!-- Page Container -->
+<div id="page-container" class="sidebar-o enable-page-overlay side-scroll page-header-glass page-header-fixed side-trans-enabled page-header-scroll">
+{{--                                enable-page-overlay side-scroll sidebar-o side-trans-enabled page-header-fixed page-header-glass page-header-scroll--}}
+    <!-- Side Overlay-->
+    <x-side-overlay/>
+    <!-- END Side Overlay -->
+
+    <!-- Sidebar -->
+    <x-admin.admin-sidebar/>
+    <!-- END Sidebar -->
+
+    <!-- Header -->
+    <x-header/>
+    <!-- END Header -->
+
+    <!-- Main Container -->
+    <main id="main-container">
+
+        <!-- Hero -->
+        <x-hero/>
+        <!-- END Hero -->
+
+        <!-- Breadcrumb -->
+        <x-admin.breadcrumb/>
+        <!-- END Breadcrumb -->
+
+        <!-- Page Content -->
+        @yield('content')
+        <!-- END Page Content -->
+    </main>
+    <!-- END Main Container -->
+
+    <!-- Footer -->
+    <x-footer/>
+    <!-- END Footer -->
+</div>
+<!-- END Page Container -->
+<x-app-footer/>
